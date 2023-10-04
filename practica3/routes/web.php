4de15aa43926c61de80b7f698ad8df33e2d0cd21
AbrilSH01 <125911949+AbrilSH01@users.noreply.github.com>
2023-10-04 15:18:00 -0600

    first commit

--- practica3/routes/web.php
+++ practica3/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::view('/', 'welcome') ->name('apodoInicio');
+Route::view('/formulario', 'formulario')->name('apodoFormulario');
+Route::view('/recuerdo', 'recuerdo')->name('apodoRecuerdos');
